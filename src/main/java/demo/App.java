6f6beb13e83c65a32736143e0b61333a3567f5ab
Sam Montour <smontour@hotmail.com>
2018-04-01 01:38:13 +0000

    Initial commit

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,20 @@
+package demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
